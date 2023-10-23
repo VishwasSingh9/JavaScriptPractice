687e1e7482862b0642a177be7dbfad74fbb93eaf
Vishwas Singh <82624400+VishwasSingh9@users.noreply.github.com>
2023-10-23 12:47:07 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,16 @@
+"use strict"; // treat all js code as newer version 
+
+// alert() // It is used in browser not in nodejs
+
+// datatypes :- 
+// number => use to represent numbers from 2 to power 52/53
+// bigint
+// boolean => true/false
+// null => standalone value
+// undefined => created but not defined
+// symbol => use to distunguish uniqueness
+
+// typeof => use to show the type of data
+console.log(typeof "vishwas"); //it is a string type of datatype
+console.log(typeof null); // null is object type datatypes in js
+console.log(typeof undefined); // undefined is undefined type of datatypes in js
